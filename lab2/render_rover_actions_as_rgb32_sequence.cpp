6f726d1ccd9286a4e78bddc9d0f30d6da7ec87bf
David Thomas <dt10@imperial.ac.uk>
2020-01-23 14:56:32 +0000

    Embedding dummy rover into render_rover to make it standalone.

--- lab2/render_rover_actions_as_rgb32_sequence.cpp
+++ lab2/render_rover_actions_as_rgb32_sequence.cpp
@@ -2,10 +2,11 @@
 
 #include "rover_action.hpp"
 
-#include "rover_dummy.hpp"
-
 #include <vector>
 #include <algorithm>
+#include <cmath>
+
+
 
 /* Represents an image over the range [-1,+1] x [-1,+1] */
 class Image
@@ -153,4 +154,73 @@ int main(int argc, const char **argv)
         }
 
     }
+}
+
+
+////////////////////////////////////////////////////////////////
+
+/* Embedded rover logic. Bit of a hack */
+
+Rover::Rover()
+{
+    m_time=0;
+    m_angle=0;
+    m_position={0,0};
+    m_speed=0;
+    m_pen_down=false;
+}
+
+Rover::~Rover()
+{
+}
+
+float Rover::get_time() const
+{
+    return m_time;
+}
+
+vector2d Rover::get_position() const
+{
+    return m_position;
+}
+
+float Rover::get_speed() const
+{
+    return m_speed;
+}
+
+float Rover::get_angle() const
+{
+    return m_angle;
+}
+
+bool Rover::get_pen_down() const
+{
+    return m_pen_down;
+}
+
+void Rover::set_speed(float speed)
+{
+    assert(speed >= 0);
+    m_speed=speed;
+}
+
+void Rover::set_angle(float angle)
+{
+    m_angle=angle;
+}
+
+void Rover::set_pen_down(bool pen_down)
+{
+    m_pen_down=pen_down;
+}
+
+void Rover::advance_time(float dt)
+{
+    vector2d p = m_position;
+    vector2d direction = vector2d{ cos(m_angle) , sin(m_angle) } ;
+    vector2d pn = m_position + direction * dt * m_speed;
+
+    m_time += dt;
+    m_position=pn;
 }
